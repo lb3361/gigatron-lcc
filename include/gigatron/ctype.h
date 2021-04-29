2ec73b316464ef42ffcfddcfbca5ec984ba3ff93
lb3361 <lb3361@nyu.edu>
2021-04-29 08:59:16 -0400

    made ctype a bit more efficient

--- include/gigatron/ctype.h
+++ include/gigatron/ctype.h
@@ -30,15 +30,15 @@ extern unsigned char _ctype[];
 
 #define isascii(c)      ((c)==((c)&0x7f))
 
-#define _isctype(c,f)   ((isascii(c) && (_ctype[c]&(f))))
+#define _isctype(c,f)   (isascii(c)?_ctype[c]&(f):0)
 
-#define	isalnum(c)	(_isctype((c),(__U|__L|__N)))
-#define	isalpha(c)	(_isctype((c),(__U|__L)))
+#define	isalnum(c)	(_isctype((c),__U|__L|__N))
+#define	isalpha(c)	(_isctype((c),__U|__L))
 #define	iscntrl(c)	(_isctype((c),__C))
 #define	isdigit(c)	(_isctype((c),__N))
-#define	isgraph(c)	(_isctype((c),(__P|__U|__L|__N)))
+#define	isgraph(c)	(_isctype((c),__P|__U|__L|__N))
 #define	islower(c)	(_isctype((c),__L))
-#define	isprint(c)	(_isctype((c),(__P|__U|__L|__N|__B)))
+#define	isprint(c)	(_isctype((c),__P|__U|__L|__N|__B))
 #define	ispunct(c)	(_isctype((c),__P))
 #define	isspace(c)	(_isctype((c),__S))
 #define	isupper(c)	(_isctype((c),__U))

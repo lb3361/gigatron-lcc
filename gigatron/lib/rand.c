c47ceb122f5992540a567020c5d79ed0a84c2c29
lb3361 <lb3361@nyu.edu>
2021-05-10 11:04:43 -0400

    added comment about srand(1975)

--- gigatron/lib/rand.c
+++ gigatron/lib/rand.c
@@ -6,8 +6,7 @@ static long seed;
 int rand(void)
 {
 	if (!init) srand(1975);
-	// Simple LCG.
-	// The multiplier comes from Steele's paper.
+	/* Simple LCG. The multiplier comes from Steele's paper. */
 	seed = seed * 0xa13fc965L + 1013904223L;
 	return ((int*)&seed)[1];
 }
@@ -16,6 +15,7 @@ void srand(unsigned int x)
 {
 	init = 1;
 	if (x == 1975) {
+		/* magic: srand(1975) mixes the gigatron entropy bytes. */
 		((char*)&seed)[0] = ((char*)6)[0];
 		((char*)&seed)[1] = ((char*)6)[1];
 		((char*)&seed)[2] = ((char*)6)[2];

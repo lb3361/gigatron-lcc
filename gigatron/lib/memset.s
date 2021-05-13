fdeca317dbb9167bf68fe6f785fa2333c83e5599
lb3361 <lb3361@nyu.edu>
2021-05-13 08:13:30 -0400

    memset now uses SYS_SetMemory_v2_54. Test in gigatron/lib/tst

--- gigatron/lib/memset.s
+++ gigatron/lib/memset.s
@@ -3,24 +3,29 @@
 # memset(d,v,l)
 
 def code0():
-    nohop()
-    label('memset');
-    tryhop(4);LDW(vLR);STW(R22);          # R8=d, R9=v, R10=l
-    LDW(R8);STW(R11);ADDW(R10);STW(R10)   # now R10 is e, R11 is d
-    LD(R9);ST(vACH);STW(R9)               # now R9 is vv
-    LDW(R10);ANDI(1);_BEQ('.1')           # if (e & 1)
-    LDW(R10);SUBI(1);STW(R10)
-    LD(R9);POKE(R10)                      #  *--e = v
-    label('.1')                           
-    LDW(R11);ANDI(1);_BEQ('.2')           # if (d & 1)
-    LD(R9);POKE(R11)                      #  *d++ = v
-    LDI(1);ADDW(R11);STW(R11)
-    label('.2')
-    XORW(R10);_BEQ('.done')
-    LDW(R9);DOKE(R11)                     # *(int*)s++ = vv
-    LDI(2);ADDW(R11);STW(R11);_BRA('.2')
+    '''version that uses Sys_SetMemory_v2_54'''
+    label('memset');                            # R8=d, R9=v, R10=l
+    tryhop(4);LDW(vLR);STW(R22)
+    LDW(R8);STW(R21)                            # save R8 into R21
+    LDWI('SYS_SetMemory_v2_54');STW('sysFn')    # prep sys
+    LD(R9);ST('sysArgs1')
+    label('.loop')
+    LDW(R8);ORI(255);ADDI(1);SUBW(R8);STW(R11)           # R11: bytes until end of page
+    LDW(R10);_BGE('.test2');SUBW(R11);_BRA('.partial')
+    label('.test2');SUBW(R11);_BLE('.final')             # if R10>R11 goto partial else final
+    label('.partial')
+    STW(R10)
+    LDW(R8);STW('sysArgs2');ADDW(R11);STW(R8)
+    LDW(R11);ST('sysArgs0')
+    SYS(54)
+    _BRA('.loop')
+    label('.final')
+    LDW(R10);_BEQ('.done');ST('sysArgs0')
+    LDW(R8);STW('sysArgs2')
+    SYS(54)
     label('.done')
-    LDW(R22);tryhop(5);STW(vLR);LDW(R8);RET();
+    LDW(R22);tryhop(5);STW(vLR);LDW(R21);RET();
+
     
 code=[
     ('EXPORT', 'memset'),

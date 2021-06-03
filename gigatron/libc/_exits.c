41fbb8de6084a3919435d07c9fae4fa88270a3ec
lb3361 <lb3361@nyu.edu>
2021-06-02 21:15:58 -0400

    split _exits in shorter pieces

--- gigatron/libc/_exits.c
+++ gigatron/libc/_exits.c
@@ -2,11 +2,22 @@
 
 extern void _exitm(int, const char*);
 
+
+static const char *_exitmsg(register int signo, register int fpeinfo)
+{
+	if (signo == SIGABRT)
+		return "Abort";
+	else if (signo != SIGFPE)
+		return 0;
+	else if (fpeinfo == 1)
+		return "Division by zero";
+	else if (fpeinfo == 2)
+		return "Floating point overflow";
+	else
+		return "Floating point exception";
+}
+
 void _exits(register int signo, register int fpeinfo)
 {
-	_exitm(20, (signo == SIGABRT) ? "Abort"
-	       :   (signo != SIGFPE) ? 0
-	       :   (fpeinfo == 1) ? "Division by zero"
-	       :   (fpeinfo == 2) ? "Floating point overflow"
-	       :   "Floating point exception" );
+	_exitm(20, _exitmsg(signo, fpeinfo));
 }

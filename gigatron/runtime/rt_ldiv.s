044ee8e39466fe8141e801461f80d8ab424b0297
lb3361 <lb3361@nyu.edu>
2021-07-03 13:53:59 -0400

    rt_ldiv: reimplemented using the shift method

--- gigatron/runtime/rt_ldiv.s
+++ gigatron/runtime/rt_ldiv.s
@@ -1,49 +1,47 @@
 
 def scope():
 
+    if False and 'has_SYS_notyetavailable' in rominfo:
+        error("Not implemented")
+        pass
 
-    def code_ldivworker():
-        # inputs
-        #  LAC:    a  dividend  [unsigned]
-        #  T0T1:   d  divisor   [unsigned, nonzero]
-        # returns
-        #  B0:     shift amount
-        #  T2T3:   quotient
-        #  LAC:    remainder<<B1
-        # uses
-        #  B1:     shift counter
-        label('__@ldivworker')
-        PUSH()
-        LDI(0);STW(B0);STW(T2);STW(T3)                     # clear
-        label('.ldw1')
-        _CALLJ('__@lcmpu_t0t1');_BLT('.ldw5')              # stop if q>a
-        _LDI(0xff80);ANDW(T0+2);_BNE('.ldw2')
-        LDW(T0+1);STW(T0+2);LD(T0);ST(T0+1);LDI(0);ST(T0)  # shift by 8 positions
-        LD(B0);ADDI(8);ST(B0);_BRA('.ldw1')
-        label('.ldw2')
-        LD(T0+3);ANDI(0xc0);_BNE('.ldw3')
-        _CALLJ('__@lshl1_t0t1')                            # shift by 1 position
-        INC(B0);_BRA('.ldw1')
-        label('.ldw3')                                     # cannot safely shift anymore
-        _CALLJ('__@lsub_t0t1');INC(T2);_BRA('.ldw1')       # subtract q from a until q>a
-        label('.ldw4')
-        INC(B1);                                           # incr shift counter
-        _CALLJ('_@_lshl1')                                 # shift a
-        _CALLJ('__@lshl1_t2t3')                            # shift q
-        _CALLJ('__@lcmpu_t0t1');_BLT('.ldw5')              # can we subtract
-        _CALLJ('__@lsub_t0t1');INC(T2);                    # yes: subtract and set low bit of q
-        label('.ldw5')
-        LD(B1);XORW(B0);LD(vACL);_BNE('.ldw4')             # shift more?
-        tryhop(2);POP();RET()
+    else:
+        # Long division using vCPU
+        #  LAC:  in: dividend  out: remainder
+        #  T0T1: in: divisor
+        #  T2T3: out: quotient
+        # Clobbers B1
 
-    module(name='rt_ldivworker.s',
-           code=[ ('EXPORT', '__@ldivworker'),
-                  ('IMPORT', '__@lsub_t0t1'),
-                  ('IMPORT', '__@lcmpu_t0t1'),
-                  ('IMPORT', '_@_lshl1'),
-                  ('IMPORT', '__@lshl1_t2t3'),
-                  ('IMPORT', '__@lshl1_t0t1'),
-                  ('CODE', '__@ldivworker', code_ldivworker) ])
+        def CallWorker():
+            _CALLJ('__@ldivworker')
+
+        def code_ldivworker():
+            label('__@ldivworker')
+            PUSH()
+            LDW(LAC);STW(T2);LDW(LAC+2);STW(T2+2)
+            LDI(0);ST(B1);STW(LAC);STW(LAC+2)
+            label('.ldiv0')
+            LDW(LAC+2);LSLW();STW(LAC+2)
+            LDW(LAC);_BGE('.ldw1');LDI(1);ORW(LAC+2);STW(LAC+2)
+            LDW(LAC);label('.ldw1');LSLW();STW(LAC)
+            LDW(T2+2);_BGE('.ldw2');LDI(1);ORW(LAC);STW(LAC)
+            LDW(T2+2);label('.ldw2');LSLW();STW(T2+2)
+            LDW(T2);_BGE('.ldw3');LDI(1);ORW(T2+2);STW(T2+2)
+            LDW(T2);label('.ldw3');LSLW();STW(T2)
+            _CALLJ('__@lcmpu_t0t1');_BLT('.ldiv1')
+            _CALLJ('__@lsub_t0t1');INC(T2)
+            label('.ldiv1')
+            INC(B1);LD(B1);XORI(32);_BNE('.ldiv0')
+            tryhop(2);POP();RET()
+
+        module(name='rt_ldivworker.s',
+               code=[ ('EXPORT', '__@ldivworker'),
+                      ('IMPORT', '__@lsub_t0t1'),
+                      ('IMPORT', '__@lcmpu_t0t1'),
+                      ('CODE', '__@ldivworker', code_ldivworker) ])
+
+        morecode = [('IMPORT', '__@ldivworker')]
+
 
     def code_ldivprep():
         nohop()
@@ -51,7 +49,8 @@ def scope():
         STW(T3);DEEK();STW(T0);
         LDW(T3);ADDI(2);DEEK();STW(T0+2);
         ORW(T0);_BNE('.ldp1')
-        LDWI(0x0104);_CALLI('_@_raise');POP() # get return address from caller
+        LDWI(0x0104);_CALLI('_@_raise')
+        POP() # get return address from caller
         label('.ldp1')
         RET()
 
@@ -65,15 +64,14 @@ def scope():
         label('_@_ldivu')
         PUSH()
         _CALLI('__@ldivprep')
-        _CALLJ('__@ldivworker')
+        CallWorker()
         LDW(T2);STW(LAC);LDW(T2+2);STW(LAC+2)
         tryhop(2);POP();RET()
 
     module(name='rt_ldivu.s',
            code=[ ('EXPORT', '_@_ldivu'),
                   ('IMPORT', '__@ldivprep'),
-                  ('IMPORT', '__@ldivworker'),
-                  ('CODE',   '_@_ldivu', code_ldivu) ] )
+                  ('CODE',   '_@_ldivu', code_ldivu) ] + morecode )
 
     def code_lmodu():
         # LMODU : LAC <- LAC % [vAC]
@@ -81,17 +79,14 @@ def scope():
         label('_@_lmodu')
         PUSH()
         _CALLI('__@ldivprep')
-        _CALLJ('__@ldivworker')
-        _CALLI('__@lshru_b0')
+        CallWorker()
         LDW(T2);STW(T0);LDW(T2+2);STW(T0+2)
         tryhop(2);POP();RET()
 
     module(name='rt_lmodu.s',
            code=[ ('EXPORT', '_@_lmodu'),
                   ('IMPORT', '__@ldivprep'),
-                  ('IMPORT', '__@ldivworker'),
-                  ('IMPORT', '__@lshru_b0'),
-                  ('CODE',   '_@_lmodu', code_lmodu) ] )
+                  ('CODE',   '_@_lmodu', code_lmodu) ] + morecode )
 
     def code_ldivsign():
         # B2 bit 7 : quotient sign
@@ -121,7 +116,7 @@ def scope():
         PUSH()
         _CALLI('__@ldivprep')
         _CALLJ('__@ldivsign')
-        _CALLJ('__@ldivworker')
+        CallWorker()
         LDW(T2);STW(LAC);LDW(T2+2);STW(LAC+2)
         LD(B2);ANDI(0x80);_BEQ('.ret')
         _CALLJ('_@_lneg')
@@ -132,9 +127,8 @@ def scope():
            code=[ ('EXPORT', '_@_ldivs'),
                   ('IMPORT', '__@ldivprep'),
                   ('IMPORT', '__@ldivsign'),
-                  ('IMPORT', '__@ldivworker'),
                   ('IMPORT', '_@_lneg'),
-                  ('CODE',   '_@_ldivs', code_ldivs) ] )
+                  ('CODE',   '_@_ldivs', code_ldivs) ] + morecode )
 
     def code_lmods():
         # LMODS : LAC <- LAC % [vAC]
@@ -143,8 +137,7 @@ def scope():
         PUSH()
         _CALLI('__@ldivprep')
         _CALLJ('__@ldivsign')
-        _CALLJ('__@ldivworker')
-        _CALLI('__@lshru_b0')
+        CallWorker()
         LDW(T2);STW(T0);LDW(T2+2);STW(T0+2)
         LD(B2);ANDI(0x80);_BEQ('.lms1')
         _CALLJ('__@lneg_t0t1')
@@ -158,12 +151,9 @@ def scope():
            code=[ ('EXPORT', '_@_lmods'),
                   ('IMPORT', '__@ldivprep'),
                   ('IMPORT', '__@ldivsign'),
-                  ('IMPORT', '__@ldivworker'),
                   ('IMPORT', '__@lneg_t0t1'),
                   ('IMPORT', '_@_lneg'),
-                  ('IMPORT', '__@lshru_b0'),
-                  ('CODE',   '_@_lmods', code_lmods) ] )
-
+                  ('CODE',   '_@_lmods', code_lmods) ] + morecode )
 
 scope()
 

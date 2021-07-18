5057d5297bfb0e3237b4956b33c53b19cb37d7b7
lb3361 <lb3361@nyu.edu>
2021-07-18 15:42:49 -0400

    non-table based ctype

--- include/gigatron/ctype.h
+++ include/gigatron/ctype.h
@@ -1,49 +1,29 @@
 #ifndef __CTYPE
 #define __CTYPE
 
-#define	__U	01
-#define	__L	02
-#define	__N	04
-#define	__S	010
-#define	__P	020
-#define	__C	040
-#define	__B	0100
-#define	__X	0200
+/* Table based ctype does not make sense in the gigatron 
+   because it requires too much contiguous memory. */
 
-enum {
-      _isalnum  = __U|__L|__N,
-      _isalpha  = __U|__L,
-      _iscntrl  = __C,
-      _isdigit  = __N,
-      _isgraph  = __P|__U|__L|__N,
-      _islower  = __L,
-      _isprint  = __P|__U|__L|__N|__B,
-      _ispunct  = __P,
-      _isspace  = __S,
-      _isupper  = __U,
-      _isxdigit = __X
-};
-
-extern unsigned char _ctype(unsigned int);
-
-#define isascii(c)      (!((c)&~0x7fU))
-#define	isalnum(c)	(_ctype(c)&_isalnum)
-#define	isalpha(c)	(_ctype(c)&_isalpha)
-#define	iscntrl(c)	(_ctype(c)&_iscntrl):q!
-#define	isdigit(c)	(_ctype(c)&_isdigit)
-#define	isgraph(c)	(_ctype(c)&_isgraph)
-#define	islower(c)	(_ctype(c)&_islower)
-#define	isprint(c)	(_ctype(c)&_isprint)
-#define	ispunct(c)	(_ctype(c)&_ispunct)
-#define	isspace(c)	(_ctype(c)&_isspace)
-#define	isupper(c)	(_ctype(c)&_isupper)
-#define	isxdigit(c)	(_ctype(c)&_isxdigit)
-
+extern int isascii(int);
+extern int isalnum(int);
+extern int isalpha(int);
+extern int iscntrl(int);
+extern int isdigit(int);
+extern int isgraph(int);
+extern int islower(int);
+extern int isprint(int);
+extern int ispunct(int);
+extern int isspace(int);
+extern int isupper(int);
+extern int isxdigit(int);
 extern int tolower(int);
 extern int toupper(int);
 
-/* Some alternatives that might evaluate c multiple times. 
+#define isascii(c)      (!((c)&~0x7fU))
+
+/* Macro alternatives that might evaluate c multiple times. 
    Using a-b?0 instead of a?b to use SUBI over _CMPWI */
+
 #define	_isalpha(c)	(((c)|0x20)>=-'A'>=0 && ((c)|0x20)-'Z'<=0)
 #define	_isdigit(c)	((c)-'0'>=0 && (c)-'9'<=0)
 #define	_islower(c)	((c)-'a'>=0 && (c)-'z'<=0)

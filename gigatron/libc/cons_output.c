68f856d7662138f9abdfc08c1099184d31c22b5f
lb3361 <lb3361@nyu.edu>
2021-10-04 18:20:57 -0400

    cons_output: remove bell by default, save bytes

--- gigatron/libc/cons_output.c
+++ gigatron/libc/cons_output.c
@@ -9,23 +9,20 @@ struct console_state_s console_state = { CONSOLE_DEFAULT_FGBG };
 static char *cons_addr(void)
 {
 	for(;;) {
-		register int cx = console_state.cx;
-		register int cy = console_state.cy;
-		register char *addr = _console_addr(cx, cy);
-		register int nl;
-		if (addr)
+		register int cx, cy, nl;
+		register char *addr;
+		if ((addr = _console_addr(cx = console_state.cx, cy = console_state.cy)))
 			return addr;
 		if (cx < 0)
 			cx = 0;
-		if (cx - console_info.ncolumns >= 0) {
+		if (console_info.ncolumns - cx <= 0) {
 			cx = 0;
 			cy += 1;
 		}
 		console_state.cx = cx;
-		nl = console_info.nlines;
 		if (cy < 0)
 			cy = 0;
-		if (cy - nl >= 0) {
+		if ((nl = console_info.nlines) - nl <= 0) {
 			console_scroll(0, nl, 1);
 			console_clear_line(cy = nl - 1);
 		}
@@ -43,7 +40,7 @@ static void cons_bell(void)
 }
 
 
-static int cons_control(register int c)
+static void cons_control(register int c)
 {
 	switch(c) {
 	case '\a':
@@ -73,7 +70,6 @@ static int cons_control(register int c)
 			console_state.controlf(c);
 		break;
 	}
-	return 1;
 }
 
 int console_print(const char *ss, register int len)
@@ -83,8 +79,10 @@ int console_print(const char *ss, register int len)
 	while (len > 0 && *s) {
 		if (n = _console_printchars(console_state.fgbg, cons_addr(), s, len))
 			console_state.cx += n;
-		else
-			n = cons_control(*s);
+		else {
+			cons_control(*s);
+			n = 1;
+		}
 		s += n;
 		len -= n;
 	}

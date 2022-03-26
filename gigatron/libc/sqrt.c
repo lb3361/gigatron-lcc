c70f89bbfd044f7484868c154a3722db27cbf5a0
lb3361 <lb3361@nyu.edu>
2022-03-26 10:31:07 -0400

    better sqrt code

--- gigatron/libc/sqrt.c
+++ gigatron/libc/sqrt.c
@@ -10,17 +10,16 @@ double sqrt(double x)
 		errno = EDOM;
 		return _fexception(_fminus);
 	} else {
-		register int i;
 		int e;
-		double w = x;
-		double z = frexp(x, &e);
-		x = 4.173075996388649989089E-1 + 5.9016206709064458299663E-1 * z;
+		register double z = frexp(x, &e);
+		register int i;
+		z = 4.173075996388649989089E-1 + 5.9016206709064458299663E-1 * z;
 		if (e & 1)
-			x = x * 1.41421356237309504880;
-		x = ldexp(x, (e >> 1));
-		for (i=0; i!=3; i++)
-			x = (x + w / x) * _fhalf;
-		return x;
+			z = z * 1.41421356237309504880;
+		z = ldexp(z, (e >> 1));
+		for (i=3; i; i--)
+			z = (z + x / z) * _fhalf;
+		return z;
 	}
 }
 

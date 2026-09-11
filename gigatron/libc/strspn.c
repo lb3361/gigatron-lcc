5c2dee2e4490be13403f66d8f5d44a85b7168ae3
lb3361 <lb3361@nyu.edu>
2026-09-11 16:41:54 -0400

    strspn no longer depends on _bitset.c (only compat now)

--- gigatron/libc/strspn.c
+++ gigatron/libc/strspn.c
@@ -1,27 +1,12 @@
 #include <string.h>
 #include <gigatron/libc.h>
 
-static unsigned char _mask[8] = { 1,2,4,8,16,32,64,128 };
-
-void
-_spnset(register char *b, register const char *set)
-{
-	register int c;
-	_bitset_clear(b, 32);
-	while (c = *set) {
-		_bitset_set(b, c);
-		set++;
-	}
-}
-
 size_t
 strspn(register const char *s0, register const char *set)
 {
-	char b[32];
 	register int c;
 	register const char *s = s0;
-	_spnset(b, set);
-	while ((c = *s0) && _bitset_test(b, c))
+	while ((c = *s0) && *(char*)__memchr2(set, (char)c, 255))
 		s0 += 1;
 	return s0 - s;
 }
@@ -29,11 +14,9 @@ strspn(register const char *s0, register const char *set)
 size_t
 strcspn(register const char *s0, register const char *set)
 {
-	char b[32];
 	register int c;
 	register const char *s = s0;
-	_spnset(b, set);
-	while ((c = *s0) && !_bitset_test(b, c))
+	while ((c = *s0) && !*(char*)__memchr2(set, (char)c, 255))
 		s0 += 1;
 	return s0 - s;
 }
